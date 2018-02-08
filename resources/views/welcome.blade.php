6700703e2892412dcc803bb0cf831a98673b30e1
Jack Whiting <hi@jackwhiting.co.uk>
2018-02-08 17:34:14 +0000

    home text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,8 @@
 
     <div class="intro-content container inner-container">
         <h1>Hey there,</h1>
-        <p>Intro text content to go here, whatever I decide to write about.</p>
+        <p><b>My name is Jack Whiting, I'm a front-end and back-end developer. Specialisng in Laravel and WordPress.</b></p>
+        <p>I run a small studio called <a href="">Lune</a>. I build brochure websites, blogs, ecommerce and applications.</p>
     </div>
 
 @endcomponent
