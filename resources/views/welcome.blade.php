8bf9d4b7ac106ae57bddb54059b224e22d13f07a
Jack Whiting <hi@jackwhiting.co.uk>
2018-02-08 17:21:02 +0000

    upgrading to laravel 5.6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,8 @@
 @component('layouts.app')
 
-    <div class="intro-content container">
-        Intro text content to go here, whatever I decide to write about.
+    <div class="intro-content container inner-container">
+        <h1>Hey there,</h1>
+        <p>Intro text content to go here, whatever I decide to write about.</p>
     </div>
-
 
 @endcomponent
