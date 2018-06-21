283c50c4a7cd14b0ec8838372c9a91d656efe943
Jack Whiting <hi@jackwhiting.co.uk>
2018-06-21 22:07:07 +0100

    removing ga, switching gosquared

--- resources/views/layouts/partials/analytics.blade.php
+++ resources/views/layouts/partials/analytics.blade.php
@@ -1,9 +1,8 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-86105963-1"></script>
 <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+  !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
+  arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+  d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+  insertBefore(d,q)}(window,document,'script','_gs');
 
-    gtag('config', 'UA-86105963-1');
+  _gs('GSN-707829-I');
 </script>
